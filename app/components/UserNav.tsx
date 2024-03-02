6b38f04ffe4886699216c4151c5070f4c365fe88
Taras Gavrysh <tg@Tarass-MacBook-Pro.local>
2024-03-02 13:18:55 +0200

    added session user data to UserNav

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -5,15 +5,17 @@ import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "@/components/ui/dropdown-menu";
 import { DropdownMenuSeparator, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function UserNav() {
+  const { data: session } = useSession()
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-10 w-10 rounded-sm">
           <Avatar className="h-10 w-10 rounded-sm">
-            <AvatarImage src="https://dqikkyvtapqvowgaepnd.supabase.co/storage/v1/object/public/user%20image/avatar.png" />
-            <AvatarFallback className="rounded-sm">Jan</AvatarFallback>
+            <AvatarImage src={session?.user?.image as string} />
+            <AvatarFallback className="rounded-sm">{session?.user?.name}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
@@ -22,7 +24,7 @@ export default function UserNav() {
         <DropdownMenuLabel>
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">User</p>
-            <p className="text-xl leading-none text-muted-foreground">aoeu@email.com</p>
+            <p className="text-xl leading-none text-muted-foreground">{session?.user?.email}</p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
